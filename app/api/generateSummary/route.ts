1ed9fef2fe818cdcca1a2fe1f041418f3a8a2f9c
hieule2k <91259565+hieule2k@users.noreply.github.com>
2023-09-02 17:21:53 +0700

    second

--- app/api/generateSummary/route.ts
+++
@@ -1,8 +0,0 @@
-// import { NextResponse } from 'next/server';
-
-// export async function POST(request: Request) {
-//   const { todos } = await request.json();
-//   console.log(todos);
-
-//   //   communicate with openAI GPT
-// }
